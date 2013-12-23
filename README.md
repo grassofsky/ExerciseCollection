1fd443ef260b66784c3e9cfa57349a7605b34ac3
stupidgrass <ZXWyue@gmail.com>
2013-12-23 23:45:10 +0800

    add exercise choose_sort

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ for example: (1-2) * (4+5) => 1 2 - 4 5 + * (ReversePolish.c)
 **Reverse the input int number**
 for example: 2342 => 2423
 
+**Quicksort**
+Given an array, one element is chosen and the others partitioned in two subsets-those less than the partition element and those greater than or equal to it
+
+**选择排序**
+From those integers that are currently unsorted, find the smallest and place it next in the sorted list.
---
+++ choose_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 主要步骤
+   1. list[i]和list[i+1],list[i+2],...,list[n]进行比较
+   2. 交换list[i]和list[min]
+*/
+
+
+#define MAX_SIZE 101
+#define SWAP(x, y, t) ((t)=(x), (x)=(y), (y)=(t))
+
+int main() {
+  int list[MAX_SIZE];
+  int i, j, tmp, min;
+
+  // 产生随机序列
+  for(i=0; i<MAX_SIZE; ++i) {
+    list[i] = rand()%1000;
+  }
+  
+  for(i=0; i<MAX_SIZE; ++i) {
+    min = i;
+    for (j=i+1; j<MAX_SIZE; ++j) {
+      if (list[j] < list[min]) { // 比较得出大于list[i]的最小值的位置
+	min = j;
+      }
+    }
+    SWAP(list[i], list[min], tmp);
+  }
+  
+  for(i=0; i<MAX_SIZE; ++i) {
+    printf("%d\t", list[i]);
+    if (i%15 == 0) {
+      printf("\n");
+    }
+  }
+  return 0;
+}
